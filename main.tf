d6868cb1404764c0ae9e3c8ad6e1873503bb3a37
Filip Dimitrov <fdimitrov@Filips-MacBook-Pro.local>
2019-08-16 16:33:31 +0300

    127

--- main.tf
+++ main.tf
@@ -1,6 +1,20 @@
-data "extip" "external_ip" {
+resource "okta_app_oauth" "test" {
+  label          = "testAcc_replace_with_uuid"
+  type           = "web"
+  grant_types    = ["implicit", "authorization_code"]
+  redirect_uris  = ["http://d.com/"]
+  response_types = ["code", "token", "id_token"]
+  issuer_mode    = "ORG_URL"
 }
 
-output "external_ip" {
-  value = "${data.extip.external_ip.ipaddress}"
+data "okta_app" "test" {
+  label = "${okta_app_oauth.test.label}"
+}
+
+data "okta_app" "test2" {
+  id = "${okta_app_oauth.test.id}"
+}
+
+data "okta_app" "test3" {
+  label_prefix = "${okta_app_oauth.test.label}"
 }
